4eec304588a1e0366c2f923cbd4fc7a130e7ee42
Rachel E.C. Smith <recsmith@rice06.stanford.edu>
2018-11-14 21:56:26 -0800

    new text data

--- scripts/gaussian_naive_bayes.py
+++ scripts/gaussian_naive_bayes.py
@@ -21,11 +21,19 @@ def main():
     y_train = []                                         
     for train_word in train_dataset.keys():
         parameters = [                                   
-            train_dataset[train_word]['wf'],             
-            train_dataset[train_word]['pl'],             
-            train_dataset[train_word]['wl'],             
-            train_dataset[train_word]['ef'],             
-            train_dataset[train_word]['tdf']             
+            train_dataset[train_word]['TF'],             
+            train_dataset[train_word]['DF'],             
+            train_dataset[train_word]['TF']*np.log(52/train_dataset[train_word]['DF']),             
+            train_dataset[train_word]['word_length']/train_dataset[train_word]['ngram_length'],
+            #train_dataset[train_word]['ngram_length'],
+            #train_dataset[train_word]['chapter'],
+            #train_dataset[train_word]['section'],
+            #train_dataset[train_word]['paragraph'],
+            (train_dataset[train_word]['sentence_loc']-0.5)**2,
+            train_dataset[train_word]['top_freq'],
+            train_dataset[train_word]['TF_mean'],
+            train_dataset[train_word]['TF_var'],
+            train_dataset[train_word]['pos']
         ]                                                
         x_train.append(parameters)                       
                                                          
@@ -45,11 +53,19 @@ def main():
     print ('Predicting...')
     for test_word in test_dataset.keys():
         parameters = [
-            test_dataset[test_word]['wf'],
-            test_dataset[test_word]['pl'],
-            test_dataset[test_word]['wl'],
-            test_dataset[test_word]['ef'],
-            test_dataset[test_word]['tdf']
+            test_dataset[test_word]['TF'],
+            test_dataset[test_word]['DF'],
+            test_dataset[test_word]['TF']*np.log(52/test_dataset[test_word]['DF']),
+            test_dataset[test_word]['word_length']/test_dataset[test_word]['ngram_length'],
+            #test_dataset[test_word]['ngram_length'],
+            #test_dataset[test_word]['chapter'],
+            #test_dataset[test_word]['section'],
+            #test_dataset[test_word]['paragraph'],
+            (test_dataset[test_word]['sentence_loc']-0.5)**2,
+            test_dataset[test_word]['top_freq'],
+            test_dataset[test_word]['TF_mean'],
+            test_dataset[test_word]['TF_var'],
+            test_dataset[test_word]['pos']
         ]
         x_test.append(parameters)
 
@@ -60,9 +76,11 @@ def main():
 
         prediction = clf.predict(np.array(parameters).reshape(1, -1))
         if prediction[0]==1 and test_word in glossary.keys():
-            print('   Passed hypothesis: ', test_word, '-- CORRECT')
+            print('   Passed hypothesis: ', test_word, '-- IN glossary!!!')
         if prediction[0]==1 and test_word not in glossary.keys():
-            print('   Passed hypothesis: ', test_word, '-- Incorrect!!!')
+            print('   Passed hypothesis: ', test_word, '-- Nope')
+        if prediction[0]==0 and test_word in glossary.keys():
+            print('   Missed: ', test_word)
 
     # Mean accuracy on the given test sample and labels
     score = clf.score(x_test, y_test)
